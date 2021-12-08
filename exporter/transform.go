e4662887e165effffded40ad9966ea3b1fe31d2c
vilaca <jvilaca@gmail.com>
2021-12-08 15:05:25 +0000

    handle undocumented sensor details

--- exporter/transform.go
+++ exporter/transform.go
@@ -7,41 +7,46 @@ var (
 		"RU": "noise",
 		"CT": "vehicle counter",
 	}
-	sensorDescriptions = map[string]map[string]string{
-		"C6H6": {"description": "benzene"},
-		"00CO": {"description": "carbon monoxide"},
-		"00HR": {"description": "relative humidity"},
-		"LAEQ": {"description": "equivalent continuous sound level"},
-		"0NO2": {"description": "nitrogen dioxide"},
-		"00NO": {"description": "nitrogen oxide"}, // undocumented
-		"00O3": {"description": "ozone"},
-		"00PA": {"description": "atmospheric pressure"},
-		"PM10": {"description": "particles with a diameter of less than 10µm"},
-		"PM25": {"description": "particles with a diameter of less than 2.5µm"},
-		"0SO2": {"description": "sulfur dioxide"},
-		"TEMP": {"description": "temperature"},
-		"0VTH": {"description": "hourly traffic volume"},
-		"00UV": {"description": "ultraviolet"},
-		"00VD": {"description": "wind direction"},
-		"00VI": {"description": "wind intensity"},
+	sensorDescriptions = map[string]string{
+		"C6H6": "benzene",
+		"00CO": "carbon monoxide",
+		"00HR": "relative humidity",
+		"LAEQ": "equivalent continuous sound level",
+		"0NO2": "nitrogen dioxide",
+		//"00NO": nitrogen oxide???, // undocumented
+		"00O3": "ozone",
+		"00PA": "atmospheric pressure",
+		//"00PP precipitation??undocumented
+		"PM10": "particles with a diameter of less than 10µm",
+		"PM25": "particles with a diameter of less than 2.5µm",
+		"0SO2": "sulfur dioxide",
+		"TEMP": "temperature",
+		"0VTH": "hourly traffic volume",
+		"00UV": "ultraviolet",
+		"00VD": "wind direction",
+		"00VI": "wind intensity",
 	}
 )
 
 func decodeSensorName(name string) (string, string, string) {
-	return name[:2], name[2:6], name[:6]
+	return sensorTypes[name[:2]], sensorDescriptions[name[2:6]], name[:6]
 }
 
 func labels(measurement sensorReading) map[string]string {
 	sensorType, description, prefix := decodeSensorName(measurement.ID)
+	if sensorType == "" {
+		sensorType = "Undocumented"
+	}
+	if description == "" {
+		description = "Undocumented"
+	}
 	labels := make(map[string]string)
-	labels["type"] = sensorTypes[sensorType]
-	labels["id"] = measurement.ID
+	labels["type"] = sensorType
+	labels["description"] = description
 	labels["prefix"] = prefix
+	labels["id"] = measurement.ID
 	labels["address"] = measurement.Address
 	labels["unit"] = measurement.Unit
 	labels["date"] = measurement.Date + " " + measurement.DateStandard
-	for name, value := range sensorDescriptions[description] {
-		labels[name] = value
-	}
 	return labels
 }
